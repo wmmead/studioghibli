c7fdf8aafac5526c4d1306afba6e452747829546
William Mead <bill@meaddesign.net>
2023-04-16 00:53:40 -0700

    updated port number

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = require('./src/app')
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log('JSONPlaceholder listening on http://localhost:' + port)
